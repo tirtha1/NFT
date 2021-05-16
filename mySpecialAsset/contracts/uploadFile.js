2950aa5c5c7ce6cf5f486042fcd193ef51f75194
tirtha1 <tjsinha123@gmail.com>
2021-05-16 09:46:41 +0530

    first changes made

---
+++ mySpecialAsset/contracts/uploadFile.js
@@ -0,0 +1,20 @@
+const pinataApiKey = "a18bce7c305b995e02e6";
+const pinataSecretApiKey = "c0952d92ea0e675864154813527047b59227408e249c610d9bef1a592517b0ac";
+const axios = require("axios");
+const fs = require("fs");
+const FormData = require("form-data");
+const pinFileToIPFS = async () => {
+  const url = `https://api.pinata.cloud/pinning/pinFileToIPFS`;
+  let data = new FormData();
+  data.append("file", fs.createReadStream("./simple.json"));
+  const res = await axios.post(url, data, {
+    maxContentLength: "Infinity", 
+    headers: {
+      "Content-Type": `multipart/form-data; boundary=${data._boundary}`,
+      pinata_api_key: pinataApiKey, 
+      pinata_secret_api_key: pinataSecretApiKey,
+    },
+  });
+  console.log(res.data);
+};
+pinFileToIPFS();
